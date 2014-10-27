8081dce3ab20588a5cdb2cfd53c556efa9a98296
Gerwin van der Lugt <gerwinvanderlugt@MacBook-Pro-van-Gerwin.local>
2014-10-27 09:46:50 +0100

    Fix formatting

--- SchetsEditor/SchetsControl.cs
+++ SchetsEditor/SchetsControl.cs
@@ -5,56 +5,70 @@ using System.Windows.Forms;
 
 namespace SchetsEditor
 {
-    public class SchetsControl : UserControl
-    {
-        private Schets schets;
-        private Color penkleur;
+	public class SchetsControl : UserControl
+	{
+		private Schets schets;
+		private Color penkleur;
 
-        public Color PenKleur 
-        {   get { return penkleur; } 
-        }
-        public SchetsControl()
-        {   this.BorderStyle = BorderStyle.Fixed3D;
-            this.schets = new Schets();
-            this.Paint += this.teken;
-            this.Resize += this.veranderAfmeting;
-            this.veranderAfmeting(null, null);
-        }
-        protected override void OnPaintBackground(PaintEventArgs e)
-        {
-        }
-        private void teken(object o, PaintEventArgs pea)
-        {   schets.Teken(pea.Graphics);
-        }
-        private void veranderAfmeting(object o, EventArgs ea)
-        {   schets.VeranderAfmeting(this.ClientSize);
-            this.Invalidate();
-        }
-        public Graphics MaakBitmapGraphics()
-        {   Graphics g = schets.BitmapGraphics;
-            g.SmoothingMode = SmoothingMode.AntiAlias;
-            return g;
-        }
-        public void Schoon(object o, EventArgs ea)
-        {   schets.Schoon();
-            this.Invalidate();
-        }
-        public void Roteer(object o, EventArgs ea)
-        {   schets.Roteer();
-            this.veranderAfmeting(o, ea);
-        }
-        public void VeranderKleur(object obj, EventArgs ea)
-        {   string kleurNaam = ((ComboBox)obj).Text;
-            penkleur = Color.FromName(kleurNaam);
-        }
-        public void VeranderKleurViaMenu(object obj, EventArgs ea)
-        {   string kleurNaam = ((ToolStripMenuItem)obj).Text;
-            penkleur = Color.FromName(kleurNaam);
-        }
+		public Color PenKleur { get { return penkleur; } }
 
-        public Bitmap Bitmap
-        {
-            get { return schets.Bitmap; }
-        }
-    }
+		public SchetsControl ()
+		{
+			this.BorderStyle = BorderStyle.Fixed3D;
+			this.schets = new Schets ();
+			this.Paint += this.teken;
+			this.Resize += this.veranderAfmeting;
+			this.veranderAfmeting (null, null);
+		}
+
+		protected override void OnPaintBackground (PaintEventArgs e)
+		{
+		}
+
+		private void teken (object o, PaintEventArgs pea)
+		{
+			schets.Teken (pea.Graphics);
+		}
+
+		private void veranderAfmeting (object o, EventArgs ea)
+		{
+			schets.VeranderAfmeting (this.ClientSize);
+			this.Invalidate ();
+		}
+
+		public Graphics MaakBitmapGraphics ()
+		{
+			Graphics g = schets.BitmapGraphics;
+			g.SmoothingMode = SmoothingMode.AntiAlias;
+			return g;
+		}
+
+		public void Schoon (object o, EventArgs ea)
+		{
+			schets.Schoon ();
+			this.Invalidate ();
+		}
+
+		public void Roteer (object o, EventArgs ea)
+		{
+			schets.Roteer ();
+			this.veranderAfmeting (o, ea);
+		}
+
+		public void VeranderKleur (object obj, EventArgs ea)
+		{
+			string kleurNaam = ((ComboBox)obj).Text;
+			penkleur = Color.FromName (kleurNaam);
+		}
+
+		public void VeranderKleurViaMenu (object obj, EventArgs ea)
+		{
+			string kleurNaam = ((ToolStripMenuItem)obj).Text;
+			penkleur = Color.FromName (kleurNaam);
+		}
+
+		public Bitmap Bitmap {
+			get { return schets.Bitmap; }
+		}
+	}
 }
